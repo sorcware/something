6379a74c2fdf8251a6f3baa69c2c8dc1cc58daee
Alex Dimmock <atedimmock@gmail.com>
2026-02-11 21:49:27 +0000

    left over from chakra ui attempt

--- api.py
+++ api.py
@@ -16,10 +16,9 @@ class QueryRequest(BaseModel):
 
 app = FastAPI()
 
-# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Vite dev server
+    allow_origins=["http://localhost:5175", "http://127.0.0.1:5175"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
